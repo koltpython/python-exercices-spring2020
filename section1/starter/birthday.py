8886705dbf7065f7aa55a2b6dc001a140206f1d6
Ahmet Uysal <auysal16@ku.edu.tr>
2020-02-05 00:22:13 +0300

    Add section 1 handout and starter files

---
+++ section1/starter/birthday.py
@@ -0,0 +1,14 @@
+"""
+Write a program that first takes the year, month (as integer), and day of the user's birth date from console.
+Then, calculate and print on which day of the year the user has been born.
+
+You can assume user will enter a valid value for all inputs.
+
+Example:
+Enter your birth year: 1998
+Enter your birth month as integer (Jan=1, Feb=2, ...): 9
+Enter your birth day of the month: 21
+You are born at the 264th day of the year 1998.
+
+Hint: You may want to utilize your solution to leapyear problem.
+"""
---
+++ section1/starter/leapyear.py
@@ -0,0 +1,31 @@
+"""
+Roman general Julius Caesar introduced the first leap years over 2000 years ago.
+But the Julian calendar had only one rule: any year evenly divisible by four would be a leap year.
+This formula produced way too many leap years. Still, it was not corrected until the introduction 
+of the Gregorian calendar more than 1500 years later.
+
+In the Gregorian calendar, three criteria must be taken into account to identify leap years:
+- The year can be evenly divided by 4;
+- If the year can be evenly divided by 100, it is NOT a leap year, unless;
+- The year is also evenly divisible by 400. Then it is a leap year.
+
+Write a program that takes a year from the user and prints whether it is a leap year or not in the Gregorian calendar.
+
+You can assume the user will enter a nonnegative integer value.
+
+Examples:
+Please enter a year: 2000
+2000 is a leap year.
+
+Please enter a year: 0
+0 is a leap year.
+
+Please enter a year: 1900
+1900 is a common year.
+
+Please enter a year: 19302
+19302 is a common year.
+
+Please enter a year: 2020
+2020 is a leap year.
+"""
