f3e643246f55c5d3d8e746d425eddc666b5a2503
Ahmet Uysal <auysal16@ku.edu.tr>
2020-03-04 10:32:56 +0300

    Update section5 starters

--- section5/starter/multiply.py
+++ section5/starter/multiply.py
@@ -10,18 +10,13 @@ Print the multiplication of elements in each tuple
 #it should input two integers and create a tuple with them. It returns a list of 
 #tuples.
 def input_tuple():
-    n = int(input("Enter the number of tuples."))
-    tuples = list()
-    for i in range(0,n):
-        first_element = int(input("First element "+"tuple number "+str(i)+" :"))
-        second_element = int(input("Second element "+"tuple number "+str(i)+" :"))
-        tuples.append((first_element,second_element))
-    return tuples
+    
 
 #takes a tuple-list as parameter and prints the multiplication of the elements in each tuple in the list
 def print_multiplication(tuples):
-    for i in tuples:
-        print(i[0]*i[1])
+    
+
+ """
+ """
+
 print("This program takes a list of tuples with two elements from user and prints the multiplication of the elements in each tuple.")
-tup = input_tuple()
-print_multiplication(tup)
--- section5/starter/subset.py
+++ section5/starter/subset.py
@@ -10,27 +10,18 @@ print NO otherwise
 """
 #It takes the length of the set as input and then takes this number of elements one by one. Returns the set.
 def take_input_set():
-    num_elements = int(input("Enter number of elements:"))
-    elements = set()
-    for i in range(0,num_elements):
-        element = int(input("Enter the element "+str(i)+" :"))
-        elements.add(element)
-    return elements
-
-#Given two sets check whether one of them is a subset of the other
-def check_is_subset(set1,set2):
-   # print(set1.difference(set2))
-    if len(set1.difference(set2)) == 0 or len(set2.difference(set1)) == 0:
-        return True
-    else:
-        return False
     
 
 
+
+
+#Given two sets check whether one of them is a subset of the other
+def check_is_subset(set1,set2):
+   
+
+
+
+"""
+"""
+
 print("This program checks whether one of the given two sets is one of the subsets of the other.")
-set1 = take_input_set()
-set2 = take_input_set()
-if check_is_subset(set1,set2):
-    print("YES")
-else:
-    print("NO")
